61c1fd29b27b9fee59ecf30a1f92cbb13e8a2e05
Jairo Braga <neto.braga08@gmail.com>
2022-11-29 20:55:25 -0300

    fix: correção do erro do maven

--- src/test/java/com/myFinanceApp/financeApp/FinanceAppApplicationTests.java
+++ src/test/java/com/myFinanceApp/financeApp/FinanceAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FinaceAppApplicationTests {
+class FinanceAppApplicationTests {
 
 	@Test
 	void contextLoads() {
